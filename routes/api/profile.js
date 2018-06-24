bbe6a38690c73a855aa14160d5c06f82ece6ab01
eric.tam <eric.m2hk@gmail.com>
2018-06-24 15:37:12 +0800

    create a route for profile get profile by handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -30,13 +30,34 @@ router.get(
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
-          return res.status(404).json(errors);
+          return res.status(404).json({ msg: "error", errors, isValid: false });
         }
         res.json(profile);
       })
-      .catch(err => res.status(404).json(err));
+      .catch(err =>
+        res.status(404).json({ msg: "error", errors: err, isValid: false })
+      );
   }
 );
+
+// @route   GET api/profile/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json({ msg: "error", errors, isValid: false });
+      }
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ msg: "error", errors: err, isValid: false })
+    );
+});
 
 // @route   POST api/pofile
 // @desc    create/update user profile
