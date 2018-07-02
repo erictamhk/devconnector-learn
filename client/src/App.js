2a6f9495856d86e2c287cc8a78a7fbcc76a48aa5
eric tam <eric.m2hk@gmail.com>
2018-07-02 20:51:32 +0800

    Logout and Conditional Navbar Links

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import setAuthToken from "./utils/setAuthToken";
 import jwt_decode from "jwt-decode";
-import { setCurrentUSer } from "./action/authActions";
+import { setCurrentUSer, logoutUser } from "./action/authActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -23,6 +23,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUSer(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current PRofile
+    // Redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
--- client/src/action/authActions.js
+++ client/src/action/authActions.js
@@ -42,9 +42,20 @@ export const loginUser = userData => dispatch => {
     );
 };
 
+// Set logged in user
 export const setCurrentUSer = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
   };
+};
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem("jwtToken");
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUSer({}));
 };
