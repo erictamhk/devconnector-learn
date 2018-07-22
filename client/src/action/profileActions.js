b3f5c7aeb5730b9fe5f44ade1010d3a76d8ab38b
eric.tam <eric.m2hk@gmail.com>
2018-07-22 14:33:02 +0800

    Add profiles page to the components get all profiles from db didn't display those profiles yet, working on later

--- client/src/action/profileActions.js
+++ client/src/action/profileActions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
@@ -90,6 +91,15 @@ export const deleteEducation = id => dispatch => {
         payload: err.response.data.errors
       });
     });
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res => dispatch({ type: GET_PROFILES, payload: res.data }))
+    .catch(err => dispatch({ type: GET_PROFILES, payload: null }));
 };
 
 // Delete account & profile
