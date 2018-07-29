8dafc7e1bd4e84bfbec2c46d85943e814ead4662
eric.tam <eric.m2hk@gmail.com>
2018-07-29 13:37:50 +0800

    add post items for showing those posts

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -14,6 +14,17 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
     case ADD_POST:
       return {
         ...state,
