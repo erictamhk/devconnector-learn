de1095a008cf2e32b644c624b6796b9138bf91c5
eric.tam <eric.m2hk@gmail.com>
2018-07-29 13:45:28 +0800

    delete post function added

--- client/src/action/postActions.js
+++ client/src/action/postActions.js
@@ -46,6 +46,24 @@ export const getPosts = () => dispatch => {
     );
 };
 
+// Delete post
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data.errors
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {
