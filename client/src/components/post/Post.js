dee8c8c9a7374e50a1e037b606769a1a4efa36ac
eric.tam <eric.m2hk@gmail.com>
2018-07-29 14:53:04 +0800

    delete the file

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import PostItem from "../posts/PostItem";
-import CommentForm from "./commentForm";
+import CommentForm from "./CommentForm";
 import Spinner from "../common/Spinner";
 import { getPost } from "../../action/postActions";
 import { Link } from "react-router-dom";
